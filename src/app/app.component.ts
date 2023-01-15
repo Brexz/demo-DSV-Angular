6b0e230df1f380fd2ed0b4f9e6519f3838cb8c9b
Brayan de la Torre <brayandelatorre@gmail.com>
2023-01-15 13:48:10 -0600

    Creation of functionality for project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  
 })
 export class AppComponent {
   title = 'demo-DSV';
+  //Variable to store id from user input
+  id = 0;
+
+  //Object to store JSON 
+  obj = "Waiting for data";
+
+  //Creation of object to make http request
+  constructor(private http:HttpClient) { }
+
+  
+  //Function that makes http request to server and store data coming from it
+  getInfo() {
+    this.http.get<any>('https://mytms.us.dsv.com/utiservices/tracking/simple/v2/json/paynumber/'+this.id).subscribe(data => {
+      this.obj=data;
+    })
+  }
+
 }
+
+
+
+
